2f14f092c9d36b6af9ce6fb8794e954ccad690e8
Grandiv <grandivfarand@gmail.com>
2024-09-06 15:39:44 +0700

    added some configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "s3.us-west-2.amazonaws.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "s3.us-west-2.amazonaws.com",
+      "www.google.com",
+      "images-ctf.baslerweb.com",
+      "graduate.northeastern.edu",
+      "img.freepik.com",
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
