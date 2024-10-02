4238e68e146b64e2f7cc9bef27b33cee3e2f1352
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2024-10-02 17:45:35 +0700

    fix: logic for create teams

--- src/app/api/teams/createTeams/route.ts
+++ src/app/api/teams/createTeams/route.ts
@@ -1,8 +1,8 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
-import { getAuthSession } from '@/lib/auth';
-import { createTeamsSchema } from '@/validators/teams';
+import { getAuthSession } from "@/lib/auth";
+import { createTeamsSchema } from "@/validators/teams";
 const generateUniqueReferalCode = async () => {
   let referalCode: number = 0;
   let isUnique = false;
@@ -10,31 +10,30 @@ const generateUniqueReferalCode = async () => {
   while (!isUnique) {
     // Generate a random 6-digit number
     referalCode = Math.floor(100000 + Math.random() * 900000);
-    
+
     // Check if the referral code already exists in the database
     const existingTeam = await prisma.team.findUnique({
-    where: { referalCode: referalCode.toString() },
+      where: { referalCode: referalCode.toString() },
     });
-    
+
     // If no team is found with this referal code, it's unique
     if (!existingTeam) {
       isUnique = true;
     }
   }
-  
+
   return referalCode;
 };
 
 export async function POST(req: Request, res: Response) {
   try {
     const session = await getAuthSession();
-    if(!session?.user){
+    if (!session?.user) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const body = await req.json();
-    const {name} = createTeamsSchema.parse(body);
-
+    const { name } = createTeamsSchema.parse(body);
     if (!name) {
       return new NextResponse("Name is required", { status: 400 });
     }
@@ -45,11 +44,23 @@ export async function POST(req: Request, res: Response) {
       data: {
         name,
         referalCode: referalCode.toString(),
+        users: {
+          connect:  {id: session.user.id as string},
+        }
       },
     });
 
-    return new NextResponse("Teams created successfully", { status: 201 });
+    //add teamid into teams array user who created the team
+    await prisma.user.update({
+      where: { id: session.user.id as string },
+      data: {
+        teams: {
+          connect: { id: newTeam.id },
+        },
+      },
+    });    
 
+    return new NextResponse("Teams created successfully", { status: 201 });
   } catch (error) {
     return new NextResponse("Error creating team", { status: 500 });
   }
